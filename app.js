a1de8290bfe035b231e1f576a268f7ad17b1d409
karanayron <karanayron@gmail.com>
2024-12-10 18:58:03 +0530

    Commited part a of phase 1, which consist of basc CRUD operation

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const path = require("path");
+const methodOverride = require("method-override");
 
 main()
 .then( () => {
@@ -16,25 +18,75 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
     res.send("Hey, I'm root");
 })
 
-app.get("/testlisting", (req, res) =>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-    sampleListing.save();
-    console.log("sample was saved");
-    res.send("Sucessful testing");
-    console.log("Sample listing data:", sampleListing);
+// app.get("/testlisting", (req, res) =>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+//     sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("Sucessful testing");
+//     console.log("Sample listing data:", sampleListing);
+
+// });
+
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
+});
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new");
+})
+
+
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show", { listing });
 
 });
 
+app.post("/listings", async (req, res) => {
+    console.log(req.body); // Debugging purpose
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+    
+});
+
+//Updatee Route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
 
 app.listen(8080, () => {
     console.log("server is listing to 8080");
